cdbf1d52721a3d7060c07ccf4c8b7e7641012d4d
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-31 15:17:37 +0700

    menuliskan main program

--- CircularQueuesArray_0079.cpp
+++ CircularQueuesArray_0079.cpp
@@ -111,3 +111,61 @@ class Queues
         }
     }
 };
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+                case '1':
+                {
+                    q.insert();
+                    break;
+                }
+
+                case '2':
+                {
+                    q.remove();
+                    break;
+                }
+
+                case '3':
+                {
+                    q.display();
+                    break;
+                }
+
+                case '4':
+                {
+                    return 0;
+                }
+
+                default:
+                {
+                    cout << "Invalid operation!" << endl;
+                    break;
+                }
+            }
+        }
+        catch  (exception& e)
+        {
+            cout << "Check for the valued entered." << endl;
+        }  
+    }
+    return 0;
+}
