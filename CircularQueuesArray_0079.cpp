b98bac452955a2e2fbb4c37431de217865d43ead
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-31 14:49:27 +0700

    menuliskan else

--- CircularQueuesArray_0079.cpp
+++ CircularQueuesArray_0079.cpp
@@ -58,6 +58,12 @@ class Queues
 			FRONT = -1;
 			REAR = -1;
         }
-
+        // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+        else
+        {
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else FRONT = FRONT + 1;
+		}
     }
 };
