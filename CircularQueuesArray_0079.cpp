ed4f99b90084d5991225c048c4c810e366929de8
asyiraafnufail <asyiraaf4@gmail.com>
2024-05-31 14:39:57 +0700

    menuliskan else untuk kembali ke awal array apabila rear pada posisi terakhir array

--- CircularQueuesArray_0079.cpp
+++ CircularQueuesArray_0079.cpp
@@ -20,20 +20,24 @@ class Queues
         cin >> num;
         cout << endl;
 
-        // cek apakah antrian penuh
+        // cek apakah antrian penuh?
         if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
         {
             cout << "\nQueue overflow\n";
             return;
         }
+        // cek apakah antrian kosong?
         if (FRONT == -1)
         {
 			FRONT = 0;
 			REAR =  0;
         }
+        // jika rear berada di posisi terakhir array, kembali ke awal array
         else
         {
-
+            if (REAR == max - 1)
+				REAR = 0;
+			else REAR = REAR + 1;
         }
     }
 };
